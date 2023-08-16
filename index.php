0cf12c914843033c2f009d791bb1a1deab91781e
Bilal Kuzey <bk1405bgl@gmail.com>
2023-08-16 17:34:27 +0200

    Begrüßung je nach Uhrzeit Anhand einer if-Bedingung

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
     echo "<a href='https://php.bk1405bgl.dev'>zurück</a><br>";
     echo "Hello World!";
+    if (date("H") > 5 AND (date("H") <= 12)) {
+        echo "<br>Guten Morgen!";
+    } else if (date("H") > 12 AND (date("H") < 18)) {
+        echo "<br>Guten Tag!";
+    } else if (date("H") > 18 AND (date("H") < 23)) {
+        echo "<br>Guten Abend!";
+    } else {
+        echo "<br>Gute Nacht!";
+    }
