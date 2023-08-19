21402cce98830c52d2253ec51eb1384ac3888c30
Bilal Kuzey <bk1405bgl@gmail.com>
2023-08-19 06:10:44 +0200

    if-Anweisungen & Variablen Grundlagen

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
     echo "<a href='https://php.bk1405bgl.dev'>zurück</a><br>";
     echo "Hello World!";
-    if (date("H") > 5 AND (date("H") <= 12)) {
+    if (date("H") > 5 && (date("H") <= 12)) {
         echo "<br>Guten Morgen!";
-    } else if (date("H") > 12 AND (date("H") < 18)) {
+    } else if (date("H") > 12 && (date("H") < 18)) {
         echo "<br>Guten Tag!";
-    } else if (date("H") > 18 AND (date("H") < 23)) {
+    } else if (date("H") > 18 && (date("H") < 23)) {
         echo "<br>Guten Abend!";
     } else {
         echo "<br>Gute Nacht!";
+    }
+    echo "<br>";
+    echo "<br>";
+    $a = 5;
+    $b = 3;
+    $c = $a + $b;
+    echo "a + b = " . $c;
+    echo "<br>";
+    echo "<br>";
+    for ($i = 1; $i <=10; $i++) {
+        echo $i . "<br>";
     }
